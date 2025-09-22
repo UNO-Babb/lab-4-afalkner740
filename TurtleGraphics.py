d6eb987ede127bde5a346799cecc7c113f66953c
afalkner740 <afalknerpersonal@gmail.com>
2025-09-21 22:38:41 -0500

    Done with part 1 and 2 of lab.

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -1,7 +1,7 @@
 #TurtleGraphics.py
-#Name:
-#Date:
-#Assignment:
+#Name: Alexa Falkner
+#Date: 9/21
+#Assignment: Turtle
 
 import turtle #needed generally but not in CodeHS
 hideturtle() #hides the default turtle in CodeHS
@@ -9,19 +9,45 @@ hideturtle() #hides the default turtle in CodeHS
 def drawSquare(myTurtle, size):
     for i in range(4):
         myTurtle.forward(size)
-        myTurtle.right(90)
+        myTurtle.left(90)
+
+def drawPolygon(bob, sides):
+    for s in range(sides):
+        bob.forward(50)
+        bob.right(360/sides)
+
+def fillCorner(alice, corner):
+    drawSquare(alice, 100)
+    
+    if corner == 1:
+        alice.begin_fill()
+        drawSquare(alice, 50)
+        alice.end_fill()
+        
+    elif corner == 2:
+        alice.forward(50)
+        alice.forward(50)
+        alice.left(90)
+        alice.begin_fill()
+        alice.forward(50)
+        drawSquare(alice,50)
+        alice.end_fill()
 
 
 def main():
     myTurtle = turtle.Turtle()
-    # drawPolygon(myTurtle, 5) #draws a pentagon
-    # drawPolygon(myTurtle, 8) #draws an octogon
-
-    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
-    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
-
-    # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
-    # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
-
+    wn = turtle.Screen()
+    wom = turtle.Turtle()
+    sizevar = 100
+    for i in range(5):
+       draw_square(wom, sizevar)
+       sizevar += 40
+       wom.penup()
+       wom.backward(20)
+       wom.right(90)
+       wom.forward(20)
+       wom.left(90)
+       wom.pendown()
+fillCorner(myTurtle,2)
 
 main()
